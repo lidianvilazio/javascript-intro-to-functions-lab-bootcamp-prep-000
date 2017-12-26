0532c1416a4014c7dd5975833add6abc4c9baf67
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-26 21:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
+if(string.toUpperCase() === "HELLO") {
+  whisper();
+} else if(string.toLowerCase() === "hello") {
+  shout();
+}
+
 function shout(string) {
   return string.toUpperCase()
 }
 
-whisper(string) {
+function whisper(string) {
   return  string.toLowerCase()
 }
