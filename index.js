978315e64c9e7bbf4ceb444e2b54b40e2dbed27f
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-26 21:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toUppercase === "HELLO") {
+  if(string === "HELLO") {
     return "YES INDEED!"
-  } else if(string.toLowerCase === "hello") {
+  } else if(string === "hello") {
     return "I can't hear you!";
   } else {
     return "I love you, too."
