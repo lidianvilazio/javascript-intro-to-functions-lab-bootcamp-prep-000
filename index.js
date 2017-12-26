0ba37964fb94507be9268ea6443c15015cd21d05
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-26 21:49:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 
 function shout(string) {
+
+}
+
+
+function whisper(string) {
+
+}
+
+
+function logShout(string) {
+
+}
+
+function logWhisper(string) {
+
+}
+
+functionsayHiToGrandma(string) {
   
 }
